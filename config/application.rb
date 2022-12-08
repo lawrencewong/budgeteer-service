ef85e25b9b32eb348f75aaa2f2b0fd120a8ad847
lawrencewong <lawrencecywong@gmail.com>
2022-12-08 11:18:48 -0500

    fix: CORS

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,13 @@ module Budgeteer
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete]
+      end
+    end
   end
 end
+
